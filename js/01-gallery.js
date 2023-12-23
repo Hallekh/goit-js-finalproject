d24563fd0ed2f1fd3a22738338e9f39580a79678
hallekh <lekhnovska.halyna@gmail.com>
2023-12-23 18:48:32 +0100

    rendered gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const container = document.querySelector(".gallery");
+
+function createMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+console.log(container);
+container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
 
 console.log(galleryItems);
