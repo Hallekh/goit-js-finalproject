f13a22b4f36cc1b4baccc3857b5b81ab5ce4839d
hallekh <lekhnovska.halyna@gmail.com>
2023-12-23 20:20:21 +0100

    added close on escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,19 +24,6 @@ container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
 
 container.addEventListener("click", handlerItemClick);
 
-function showItem({ original, description }) {
-  const instance = basicLightbox.create(`
-      <div class="modal">
-          <img
-          src="${original}"
-          alt="${description}"
-          />
-      </div>
-  `);
-
-  instance.show();
-}
-
 function handlerItemClick(event) {
   event.preventDefault();
   if (event.currentTarget === event.target) {
@@ -53,4 +40,34 @@ function handlerItemClick(event) {
   showItem(itemData);
 }
 
+function showItem({ original, description }) {
+  const instance = basicLightbox.create(
+    `
+        <div class="modal">
+            <img
+            src="${original}"
+            alt="${description}"
+            />
+        </div>
+    `,
+    {
+      handler: null,
+      onShow(instance) {
+        this.handler = onEscape.bind(instance);
+        document.addEventListener("keydown", this.handler);
+      },
+      onClose() {
+        document.removeEventListener("keydown", this.handler);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function onEscape({ code }) {
+  if (code === "Escape") {
+    this.close();
+  }
+}
 console.log(galleryItems);
